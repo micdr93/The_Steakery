5e1e2cef97545aa5a8bc3a3e9288da0a46566eba
Michael Drew <michaeldrew_@outlook.com>
2024-09-12 12:11:04 +0000

    added about to settings.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from the_steakery import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
