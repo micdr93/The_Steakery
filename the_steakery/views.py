bf809fddc9826f89a4925f60e67d28f9038f64e9
Michael Drew <michaeldrew_@outlook.com>
2024-09-12 12:15:11 +0000

    set up about url

--- the_steakery/views.py
+++ the_steakery/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("The Steakery")
+from django.contrib import admin
+from django.urls import path, include
+from the_steakery import views as index_views
+from about import views as about_views
+
+urlpatterns = [
+    path('hello/', index_views.index, name='index'),
+    path('about/', about_views.about_me, name='about'),
+    path('admin/', admin.site.urls),
+]
