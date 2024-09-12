f485cd1689628b32a4728d1cb277530716d000f0
Michael Drew <michaeldrew_@outlook.com>
2024-09-12 12:17:56 +0000

    fixed views and urls

--- the_steakery/views.py
+++ the_steakery/views.py
@@ -1,10 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from the_steakery import views as index_views
-from about import views as about_views
+def index(request):
 
-urlpatterns = [
-    path('hello/', index_views.index, name='index'),
-    path('about/', about_views.about_me, name='about'),
-    path('admin/', admin.site.urls),
-]
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
